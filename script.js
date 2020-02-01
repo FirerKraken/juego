93238886eeade2a9859f3bc34083b831544f0e58
Glitch (rural-lace-d9aw60ek2v) <none>
2020-02-01 12:49:11 +0000

    Animations

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const WIDTH = 400;
-const HEIGHT = 200;
-const TILE_SIZE = 16;
+const HEIGHT = 300;
+const TILE_SIZE = 32;
 
 let ctx;
 
-let tiles, enemies;
+let tiles, enemies, dwarf;
 
 let map = [
   [0,   1,  1,   1,  1,  1,  1,  1,  1, 0, 10],
@@ -29,6 +29,9 @@ let props = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
 ]
+
+let lastRefresh = 0;
+
 function getCanvasContext(){
   // get element -- getElementById
   let canvas = document.getElementById("canvas");
@@ -45,11 +48,13 @@ function getCanvasContext(){
 
 function loadAssets() {
   tiles = new Image();
-  tiles.src = 'https://cdn.glitch.com/af16cf14-1720-46d1-ab6d-cfe1c2a490b0%2Fsewer.png?v=1579944579018';
+  tiles.src = 'https://cdn.glitch.com/af16cf14-1720-46d1-ab6d-cfe1c2a490b0%2Fsewer2x.png?v=1580555366755';
   
   enemies = new Image();
   enemies.src = 'https://cdn.glitch.com/af16cf14-1720-46d1-ab6d-cfe1c2a490b0%2Fcritters.png?v=1579945607002';
   
+  dwarf = new Image();
+  dwarf.src ='https://cdn.glitch.com/af16cf14-1720-46d1-ab6d-cfe1c2a490b0%2Fdwarf.png?v=1580559174927';
 }
 
 /***************************
@@ -62,9 +67,76 @@ function init() {
   // Get a canvas context
   getCanvasContext();
   
+  
+  
+  document.addEventListener('keypress', function(e) {
+    console.log(e.key); 
+    
+    if(e.key === 'a' ){
+      dwarfCol = dwarfCol - 1; 
+    }
+    if(e.key === 'd' ){
+      dwarfCol = dwarfCol + 1; 
+    }
+    if(e.key === 'w' ){
+      dwarfRow = dwarfRow - 1; 
+    }
+    if(e.key === 's' ){
+      dwarfRow = dwarfRow + 1; 
+    }
+  })
+  
+  requestAnimationFrame(loop)
+  
+}
+
+let x = 68
+let timeSinceLastFrameChange = 0
+
+function drawLamp(delta){
+  timeSinceLastFrameChange = timeSinceLastFrameChange + delta;
+  drawTileNum(x, 5, 10)
+  
+  if(timeSinceLastFrameChange>150){
+    x = x + 1;
+    timeSinceLastFrameChange = 0
+  }                               
+  
+  if(x > 70){
+    x = 68
+  }
+}
+
+let c = 0;
+let t = 0;
+let dwarfRow = 5
+let dwarfCol = 0
+function drawCharacter(delta){
+  t = t + delta
+  drawDwarf(0, c, dwarfRow, dwarfCol);
+  if(t > 100){
+    c = c + 1;
+    t = 0
+  }
+  if(c > 4){
+    c = 0 
+  }
+}
+function loop(time){
+  let delta = time - lastRefresh;
+  
+  ctx.clearRect(0,0,WIDTH, HEIGHT);
+  
   drawMap();
   drawProps();
+  drawLamp(delta);
+  drawCharacter(delta);
+  
+  requestAnimationFrame(loop);
+  
+  lastRefresh = time;
 }
+
 function drawMap()  {
   for(let r = 0; r < map.length; r = r + 1) { 
     for(let c = 0; c < map[0].length ; c = c + 1) {
@@ -99,13 +171,27 @@ function getTileCol(tile){
   return tile % 16;
 }
 
+function drawTileNum(n, dr, dc) {
+  let r = getTileRow(n);
+  let c = getTileCol(n);
+  drawTile(r, c, dr, dc) 
+}
+
+function drawDwarf(sr, sc, dr, dc) {
+  ctx.drawImage(dwarf, 
+                sc * TILE_SIZE, sr * TILE_SIZE, TILE_SIZE, TILE_SIZE, 
+                dc * TILE_SIZE, dr * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
 function drawTile(sr, sc, dr, dc) {
   ctx.drawImage(tiles, 
                 sc * TILE_SIZE, sr * TILE_SIZE, TILE_SIZE, TILE_SIZE, 
                 dc * TILE_SIZE, dr * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
+
 window.onload = () => {
   loadAssets();
   setTimeout(init, 1000);
 }
+
